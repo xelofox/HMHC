1e09a285ef90c66a4068fb4504c111b0bae23020
xelofox <38127459+xelofox@users.noreply.github.com>
2020-11-17 10:19:10 +0100

    plop

---
+++ Function/B_abcd7.m
@@ -0,0 +1,3 @@
+function B=B_abcd7(a0,a1,b0,b1,c0,c1,d0,d1)
+B=b0*c0*d0*(a1 - a0) + a0*c0*d0*(b1 - b0) + a0*b0*d0*(c1 - c0) + a0*b0*c0*(d1 - d0);
+end
---
+++ Function/B_abcd8.m
@@ -0,0 +1,3 @@
+function B=B_abcd8(a0,a1,b0,b1,c0,c1,d0,d1)
+B=a0*b0*c0*d0;
+end
