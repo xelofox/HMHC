016a71c57c798d107dee9c17172d849f7ec31757
xelofox <38127459+xelofox@users.noreply.github.com>
2020-11-22 18:39:19 +0100

    plop

--- Animation/Function/trans_rot.m
+++ Animation/Function/trans_rot.m
@@ -2,7 +2,7 @@
 
 function [X1,Y1,Z1]=trans_rot(X,Y,Z,q)
 X1=X; Y1=Y; Z1=Z;
-R=rot_x(-q(4)*pi/180)*rot_y(-q(5)*pi/180)*rot_z(-q(6)*pi/180);
+R=rot_x(q(4)*pi/180)*rot_y(q(5)*pi/180)*rot_z(q(6)*pi/180);
 %R=eye(3);
 for i=1:size(X,1)
     for j=1:size(X,2)
